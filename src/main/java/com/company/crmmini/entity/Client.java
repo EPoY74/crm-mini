91a051f481d18bbfec3e58a090d06410b728d225
eugenii_petrov_asus552_linux <epoy74@gmail.com>
2025-09-20 23:16:19 +0500

    first commit

---
+++ src/main/java/com/company/crmmini/entity/Client.java
@@ -0,0 +1,125 @@
+package com.company.crmmini.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "CLIENT", indexes = {
+        @Index(name = "IDX_CLIENT", columnList = "CLIENTS_NAME")
+})
+@Entity
+public class Client {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @NotBlank(message = "Enter customer's name")
+    @InstanceName
+    @Column(name = "CLIENTS_NAME", nullable = false)
+    @NotNull
+    private String clientsName;
+
+    @Email(message = "Enter customer's e-mail")
+    @Column(name = "CLIENT_EMAIL", nullable = false)
+    @NotNull
+    private String clientEmail;
+
+    @NotBlank(message = "Enter customer's phone number")
+    @Column(name = "CLIENT_PHONE", nullable = false)
+    @NotNull
+    private String clientPhone;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE")
+    private OffsetDateTime createdDate;
+
+    @LastModifiedBy
+    @Column(name = "LAST_MODIFIED_BY")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE")
+    private OffsetDateTime lastModifiedDate;
+
+    public String getClientPhone() {
+        return clientPhone;
+    }
+
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
+    }
+
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    public String getClientsName() {
+        return clientsName;
+    }
+
+    public void setClientsName(String clientsName) {
+        this.clientsName = clientsName;
+    }
+
+    public OffsetDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(OffsetDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public OffsetDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(OffsetDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+}
